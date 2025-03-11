c307f44f6247bb80739a492ef3e2ee56dae63dcd
Florescu Denis <119164534+Lamayanul@users.noreply.github.com>
2025-03-11 12:39:01 +0200

    cladire + miscare npc
    Corpul D aproape complet, fara acoperis, denumiri clase + miscarea npc-ului in clasa

--- Teacher's Game/Scripturi/enemy.gd
+++ Teacher's Game/Scripturi/enemy.gd
@@ -16,21 +16,52 @@ var lastPosition=Vector2(0,1)
 @onready var player_hitbox = get_node("/root/world/player/player_hitbox")
 @onready var enemy_icon = $"../player/CanvasLayer/healthbar_enemy/enemy_icon"
 @onready var healthbar_enemy = $"../player/CanvasLayer/healthbar_enemy"
+var interact=false
+@export var target: Node2D = null
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
 
 
 func _ready():
 	healthbar_enemy.value=0
-	$ChangeDirection.start()
+	#$ChangeDirection.start()
 	add_to_group("enemy_hitbox")
+	call_deferred("seeker_setup")
 	
 
 
 func _physics_process(_delta):
-	velocity = moveDirection * MoveSpeed
-	movement()
-	move_and_slide()
+	if is_instance_valid(target):
+		navigation_agent_2d.target_position = target.global_position
+	
+	if navigation_agent_2d.is_navigation_finished():
+		return
+
+	
+	var current_agent_position = global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var new_velocity = current_agent_position.direction_to(next_path_position) * Speed
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
 		
+	move_and_slide()
+	movement()  
+
+		
+	#velocity = moveDirection * MoveSpeed
+	#movement()
+	#move_and_slide()
+	
+	
+
+
+func seeker_setup():
+	await get_tree().physics_frame
+	if target:
+		navigation_agent_2d.target_position = target.global_position
 
 
 func select_new_direction():
@@ -41,7 +72,6 @@ func select_new_direction():
 	).normalized()
 
 func movement():
-
 	if velocity!=Vector2.ZERO:
 		if abs(velocity.x) > abs(velocity.y):
 			# Dacă mișcarea pe axa X este dominantă
@@ -71,3 +101,20 @@ func _on_color_timeout():
 func _on_change_direction_timeout():
 	select_new_direction()
 	
+func _input(event):
+	if Input.is_action_just_pressed("interact") and interact:
+		moveDirection = Vector2.ZERO
+		animated_sprite_2d.play("idle") 
+
+
+func _on_atack_zone_body_entered(body: Node2D) -> void:
+	interact=true
+
+
+func _on_atack_zone_body_exited(body: Node2D) -> void:
+	interact=false
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	pass
--- Teacher's Game/Scripturi/tile_map.gd
+++ Teacher's Game/Scripturi/tile_map.gd
@@ -1,8 +1,50 @@
 extends TileMap
 class_name tilemap
 
+@onready var player = get_node("/root/world/player")
+var etaj = 0  
+
 func _ready():
 	pass
-	
+
 func _process(delta):
+	if player and is_instance_valid(player):
+		pass
+
+
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and etaj == 0:
+		etaj = 1  
+		player.set_collision_layer(2)
+		player.set_collision_mask(2)
+		switch_floor()
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and etaj == 1:
+		etaj = 0  
+		player.set_collision_layer(1)
+		player.set_collision_mask(1)
+		switch_floor()
+
+
+func switch_floor():
+	if etaj == 0:
+		$ground_floor2.visible = true
+		$items.visible = true
+		$"../TileMap2/first_floor".visible = false
+		$"../TileMap2/items_2".visible=false
+		player.z_index=0
+		
+	else:
+		$ground_floor2.visible = false
+		$items.visible = false
+		$"../TileMap2/first_floor".visible = true
+		$"../TileMap2/items_2".visible=true
+		player.z_index=2
+		
+		
+
+
+func _on_area_principal_door(body: Node2D) -> void:
 	pass
